8757bf19579640a9dc52fbbed93527a5230d4045
Subhajit <subha.pramanik1212@gmail.com>
2024-03-11 01:41:22 +0530

    Code commited for final push

---
+++ client/src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDDgQ_6OFVVJCLlZhZvbc8RyQ8YstBv6Bc",
+  authDomain: "podcast-app-73175.firebaseapp.com",
+  projectId: "podcast-app-73175",
+  storageBucket: "podcast-app-73175.appspot.com",
+  messagingSenderId: "298020745232",
+  appId: "1:298020745232:web:0e74dd36c6060f20995a95"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
+
